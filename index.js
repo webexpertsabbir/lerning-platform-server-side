e61f55296371c7dd4b52b6afeb7930d6d1110471
webexpertsabbir <webexpertsabbir@gmail.com>
2022-10-26 23:28:25 +0600

    coursess data

--- index.js
+++ index.js
@@ -7,6 +7,8 @@ app.use(cors());
 
 const courses =  require('./Data/course.json');
 
+const singel = require('./Data/courses.json');
+
 app.get('/', (req, res) => {
     res.send('Mark Api Running');
 })
@@ -14,6 +16,11 @@ app.get('/', (req, res) => {
 app.get('/course', (req, res) => {
     res.send(courses);
 })
+app.get('/course/:id',(req, res) =>{
+    const id = req.params.id;
+    const selectedCourse = singel.find(n => n._id === id);
+    res.send(selectedCourse);
+} )
 
 app.listen(port, () => {
     console.log('Mark Server running on port', port);
